cb1e7154640825bd1e1b018728420509832d6664
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-06-13 15:18:46 -0300

    constantes e variaveis

---
+++ MOD_2/10_constantes.rb
@@ -0,0 +1,15 @@
+# No ruby para variavel CONSTANTE usa-se LETRA MAIUSCULA 
+# variável normal letra minusculas 
+
+=begin
+
+irb(main):001> NOME = "Rails"
+=> "Rails"
+irb(main):002> NOME.class
+=> String
+irb(main):003> NOME = "Spring"
+(irb):3: warning: already initialized constant NOME
+(irb):1: warning: previous definition of NOME was here
+=> "Spring"
+
+=end
