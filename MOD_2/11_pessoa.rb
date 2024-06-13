d8e91e9f55cd8780b378ccaa2e5d32a4839e6493
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-06-13 16:12:13 -0300

    initialize

--- MOD_2/11_pessoa.rb
+++ MOD_2/11_pessoa.rb
@@ -1,9 +1,25 @@
 class Pessoa
+
+    # é um método especial que serve para indicarmos o que a classe deve fazer ao ser instanciada/inicializada
+    def initialize (cont = 1)
+        cont.times do |i|
+            puts "Inicializando... #{i}"
+        end
+    end
+
   def falar(nome = 'pessoal') # valor padrão
     "Olá, #{nome}!"
   end
+
 end
 
-p = Pessoa.new
-puts p.falar # pessoal
-puts p.falar('Isa')
+p1 = Pessoa.new
+puts p1.falar # pessoal
+puts p1.falar('Isa')
+
+p2 = Pessoa.new(5)
+
+# Inicializando...
+# Olá, pessoal!
+# Olá, Isa!
+# Inicializando...
