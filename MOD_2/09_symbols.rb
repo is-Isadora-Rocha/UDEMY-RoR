f7bcad0661e3c4054faa6518ec68332b99c3695d
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-06-13 15:10:03 -0300

    :symbols

---
+++ MOD_2/09_symbols.rb
@@ -0,0 +1,17 @@
+puts 'Ruby'.object_id # => 60
+puts 'Ruby'.object_id # => 80
+puts 'Ruby'.object_id # => 100
+puts 'Ruby'.object_id # => 120
+puts '===================='
+puts :ruby.object_id # => 997468
+puts :ruby.object_id # => 997468
+puts :ruby.object_id # => 997468
+puts :ruby.object_id # => 997468
+
+# Símbolos são muito usados em situações onde precisamos de um identificador pois eles garantem que seu uso
+# não implacará na criação de novos objetos sempre que usados. 
+
+# hashes adoram símbolos: 
+
+h = { :curso => "Rails"}
+h = { curso: "Rails "}
